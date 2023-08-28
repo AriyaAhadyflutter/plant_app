7bb802d4bb7b2bf6fea6324f50d15d4d03c3d4c9
ariaProgrammerFlutter <87ariyaahady@gmail.com>
2023-08-28 16:23:12 +0330

    add constants

---
+++ lib/const/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static Color primarycolor = Colors.green;
+  static Color blackcolor = Colors.green;
+  String title1 = 'گیاهان را بهتر از قبل درک کن ';
+  String description1 = 'در مورد نگهداری گیاهان میتونی اطلاهات کسب کنی ';
+  String title2 = 'با گیاه های جدید اشنا شو ';
+  String description2 = 'رز مشکی یا گل رز دوست داری؟ اینجا میتونی پیداش کنی';
+  String title3 = 'با یک گل بهار نمیشه گل بکار ';
+  String description3= 'هر گلی نیاز داری تو این اپلیکیشن پیدا میکنی ';
+}
